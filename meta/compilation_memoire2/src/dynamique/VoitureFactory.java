c6b5e68869efe506ec913e6aeed58952e444ae1b
Dyzing <guillaume.trem@outlook.fr>
2021-11-23 16:36:27 -0500

    Ajout de l'affichage Réflexion

--- meta/compilation_memoire2/src/dynamique/VoitureFactory.java
+++ meta/compilation_memoire2/src/dynamique/VoitureFactory.java
@@ -9,7 +9,6 @@ public class VoitureFactory {
     public static Voiture buildVoiture(ModeConstruction mode, boolean sport,int vitesse) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         try {
             if (mode == ModeConstruction.INSTANCIATION) {
-                System.out.println("INSTANCIATION");
                 //INSTACIATION
                 if (sport) {
 
@@ -25,7 +24,6 @@ public class VoitureFactory {
 
 
             } else if (mode == ModeConstruction.REFLEXION) {
-                System.out.println("REFLEXION");
                 Scanner sc = new Scanner(System.in);
                 System.out.print("Nom de la Classe > ");
                 String nomClasse = sc.next();
@@ -35,12 +33,68 @@ public class VoitureFactory {
 
                     Class maClasse = Class.forName(nomClasse);
                     Object o1 = maClasse.getDeclaredConstructor().newInstance();
+
+                    Constructor[] constructeurs = maClasse.getDeclaredConstructors();
+                    Method[] methodes = maClasse.getDeclaredMethods();
+                    Field[] attributs = maClasse.getDeclaredFields();
+
+                    System.out.println(" ");
+
+                    for (Constructor construc: constructeurs
+                    ) {
+                        System.out.println("Constructeur : " + construc.toString());
+                    }
+
+                    System.out.println(" ");
+
+                    for (Method meth: methodes
+                    ) {
+                        System.out.println("Méthodes : " + meth.toString());
+                    }
+
+                    System.out.println(" ");
+
+                    for (Field attr: attributs
+                    ) {
+                        System.out.println("Attributs : " + attr.toString());
+                    }
+
+                    System.out.println("-------------------------------------------------------------");
+
                     return (MetaVoitureSport) o1;
 
                 } else {
 
                     Class maClasse = Class.forName(nomClasse);
                     Object o2 = maClasse.getDeclaredConstructor(int.class).newInstance(vitesse);
+
+                    Constructor[] constructeurs = maClasse.getDeclaredConstructors();
+                    Method[] methodes = maClasse.getDeclaredMethods();
+                    Field[] attributs = maClasse.getDeclaredFields();
+
+                    System.out.println(" ");
+
+                    for (Constructor construc: constructeurs
+                    ) {
+                        System.out.println("Constructeur : " + construc.toString());
+                    }
+
+                    System.out.println(" ");
+
+                    for (Method meth: methodes
+                    ) {
+                        System.out.println("Méthodes : " + meth.toString());
+                    }
+
+                    System.out.println(" ");
+
+                    for (Field attr: attributs
+                    ) {
+                        System.out.println("Attributs : " + attr.toString());
+                    }
+
+                    System.out.println("-------------------------------------------------------------");    
+
                     return (MetaVoiture) o2;
                 }
 
